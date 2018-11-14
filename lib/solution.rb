60aa03db36ef0c2dad4cbe3a5b27843f7fa99553
Skylar Salerno <skylar.salerno@gmail.com>
2018-11-14 10:01:23 -0500

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,16 @@
 def clock_angle(time)
-    # code your solution here
+time=time.split(':')
+hour=time[0].to_i+time[1].to_f/60
+min=time[1].to_i
+h_degree=30*hour
+m_degree=6*min
+if (h_degree - m_degree).abs > 180
+  if h_degree > m_degree
+    (360-h_degree + m_degree).abs
+  else
+    (360-m_degree + h_degree).abs
+  end
+else
+  (h_degree - m_degree).abs
+end
 end
